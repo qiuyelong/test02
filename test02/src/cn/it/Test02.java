98558fc9e37a436cece775281b411b52c3c1392e
qyl <qiuyelongyx>
2018-12-01 01:07:45 +0800

    三次master冲突解决

--- test02/src/cn/it/Test02.java
+++ test02/src/cn/it/Test02.java
@@ -4,8 +4,12 @@ public class Test02 {
     public static void main(String[] args) {
         System.out.println("jiuyao");
         System.out.println("genni");
+
         System.out.println("webchangchongtu");
         System.out.println("webchangchongtu2");
+
+        System.out.println("webchangchongtu3");
+
         System.out.println("webchange");
         System.out.println("qiuchange02");
         
