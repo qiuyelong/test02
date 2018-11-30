bce5cee9a471e33f15da60bc0a0b750ea40e98c6
qiuyelong <44260422+qiuyelong@users.noreply.github.com>
2018-11-30 23:27:54 +0800

    webchange01
    web change01

--- test02/src/cn/it/Test02.java
+++ test02/src/cn/it/Test02.java
@@ -4,5 +4,6 @@ public class Test02 {
     public static void main(String[] args) {
         System.out.println("jiuyao");
         System.out.println("genni");
+        System.out.println("webchange");
     }
 }
