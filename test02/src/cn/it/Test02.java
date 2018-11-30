9a8b3c8b5cd9354c93a0f14e13a6f8c0d0513f53
qiuyelong <44260422+qiuyelong@users.noreply.github.com>
2018-12-01 01:04:24 +0800

    webchongtu2
    chongtu2

--- test02/src/cn/it/Test02.java
+++ test02/src/cn/it/Test02.java
@@ -4,6 +4,7 @@ public class Test02 {
     public static void main(String[] args) {
         System.out.println("jiuyao");
         System.out.println("genni");
+        System.out.println("webchangchongtu");
         System.out.println("webchange");
         System.out.println("qiuchange02");
     }
